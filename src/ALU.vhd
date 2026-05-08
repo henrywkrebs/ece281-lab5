1df32d39f41d0ef62d57e937978871ff4b5227dd
henrywkrebs <henrywkrebs@gmail.com>
2026-05-07 19:44:30 -0600

    I have the ALU personally

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -39,26 +39,26 @@ end ALU;
 
 architecture Behavioral of ALU is
     signal w_result  : std_logic_vector(7 downto 0);
-    signal w_sum9    : std_logic_vector(8 downto 0);
     signal w_B_op    : std_logic_vector(7 downto 0);
     signal w_cin     : std_logic;
-    signal w_sum8    : std_logic_vector(7 downto 0);
+    signal w_sum9    : std_logic_vector(8 downto 0);
+    signal w_sum7    : std_logic_vector(7 downto 0);
     signal w_cout    : std_logic;
     signal w_carry6  : std_logic;
 begin
 
-    w_cin   <= i_op(0);
-    w_B_op  <= not i_B when i_op(0) = '1' else i_B;
+    w_cin  <= i_op(0);
+    w_B_op <= not i_B when i_op(0) = '1' else i_B;
 
-    w_sum9  <= std_logic_vector(('0' & unsigned(i_A)) + ('0' & unsigned(w_B_op)) + (x"0" & w_cin));
-    w_sum8  <= w_sum9(7 downto 0);
-    w_cout  <= w_sum9(8);
+    w_sum9 <= std_logic_vector(('0' & unsigned(i_A)) + ('0' & unsigned(w_B_op)) + (x"00" & w_cin));
+    w_sum7 <= std_logic_vector(('0' & unsigned(i_A(6 downto 0))) + ('0' & unsigned(w_B_op(6 downto 0))) + (x"0" & w_cin));
 
-    w_carry6 <= std_logic_vector(('0' & unsigned(i_A(6 downto 0))) + ('0' & unsigned(w_B_op(6 downto 0))) + (x"0" & w_cin))(7);
+    w_cout   <= w_sum9(8);
+    w_carry6 <= w_sum7(7);
 
-    w_result <= w_sum8          when (i_op = "000" or i_op = "001") else
-                i_A and i_B     when i_op = "010" else
-                i_A or  i_B     when i_op = "011" else
+    w_result <= w_sum9(7 downto 0)  when (i_op = "000" or i_op = "001") else
+                i_A and i_B          when i_op = "010" else
+                i_A or  i_B          when i_op = "011" else
                 (others => '0');
 
     o_result <= w_result;
