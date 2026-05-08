cd9f2c44b41adf8a28e07bece730509db9d8fd10
Henry Krebs <162069017+henrywkrebs@users.noreply.github.com>
2026-05-07 21:32:21 -0600

    Update sevenseg_decoder.vhd

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -10,27 +10,27 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity sevenseg_decoder is
     Port ( i_hex : in STD_LOGIC_VECTOR (3 downto 0);
-           o_seg : out STD_LOGIC_VECTOR (6 downto 0));
+           o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
 begin
     with i_hex select
-        o_seg <= "0000001" when "0000",
-                 "1001111" when "0001",
-                 "0010010" when "0010",
-                 "0000110" when "0011",
-                 "1001100" when "0100",
-                 "0100100" when "0101",
-                 "0100000" when "0110",
-                 "0001111" when "0111",
-                 "0000000" when "1000",
-                 "0000100" when "1001",
-                 "0001000" when "1010",
-                 "1100000" when "1011",
-                 "0110001" when "1100",
-                 "1000010" when "1101",
-                 "0110000" when "1110",
-                 "0111000" when "1111",
-                 "1111111" when others;
+        o_seg_n <= "0000001" when "0000",
+                   "1001111" when "0001",
+                   "0010010" when "0010",
+                   "0000110" when "0011",
+                   "1001100" when "0100",
+                   "0100100" when "0101",
+                   "0100000" when "0110",
+                   "0001111" when "0111",
+                   "0000000" when "1000",
+                   "0000100" when "1001",
+                   "0001000" when "1010",
+                   "1100000" when "1011",
+                   "0110001" when "1100",
+                   "1000010" when "1101",
+                   "0110000" when "1110",
+                   "0111000" when "1111",
+                   "1111111" when others;
 end Behavioral;
