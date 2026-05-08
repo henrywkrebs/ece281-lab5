c63e4de988f3cfff974f877c27c725750ed398d4
Henry Krebs <162069017+henrywkrebs@users.noreply.github.com>
2026-05-07 21:30:02 -0600

    Update sevenseg_decoder.vhd

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -1,31 +1,8 @@
-----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
--- 
--- Create Date: 02/19/2025 09:31:30 PM
--- Design Name: 
--- Module Name: sevenseg_decoder - Behavioral
--- Project Name: 
--- Target Devices: 
--- Tool Versions: 
--- Description: 
--- 
--- Dependencies: 
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
--- 
-----------------------------------------------------------------------------------
-
-
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
-
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
 --use IEEE.NUMERIC_STD.ALL;
-
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx leaf cells in this code.
 --library UNISIM;
@@ -37,30 +14,23 @@ entity sevenseg_decoder is
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
-
-signal w_seg : std_logic_vector(6 downto 0) := "0000000";
-
 begin
-
-    -- LUT for w_seg based on prelab truth table
-    with i_Hex select
-    w_seg   <=  "1111110" when x"0",
-                "0110000" when x"1",
-                "1101101" when x"2",
-                "1111001" when x"3",
-                "0110011" when x"4",
-                "1011011" when x"5",
-                "1011111" when x"6",
-                "1110000" when x"7",
-                "1111111" when x"8",
-                "1110011" when x"9",
-                "1110111" when x"A",
-                "0011111" when x"B",
-                "0001101" when x"C",
-                "0111101" when x"D",
-                "1001111" when x"E",
-                "1000111" when x"F",
-                "0000000" when others;
-        
-    
+    with i_hex select
+        o_seg <= "0000001" when "0000",
+                 "1001111" when "0001",
+                 "0010010" when "0010",
+                 "0000110" when "0011",
+                 "1001100" when "0100",
+                 "0100100" when "0101",
+                 "0100000" when "0110",
+                 "0001111" when "0111",
+                 "0000000" when "1000",
+                 "0000100" when "1001",
+                 "0001000" when "1010",
+                 "1100000" when "1011",
+                 "0110001" when "1100",
+                 "1000010" when "1101",
+                 "0110000" when "1110",
+                 "0111000" when "1111",
+                 "1111111" when others;
 end Behavioral;
