031c68ae10d247a23b786dbb4f0caa604724b0a0
Henry Krebs <162069017+henrywkrebs@users.noreply.github.com>
2026-05-07 21:37:26 -0600

    Update sevenseg_decoder.vhd

--- src/sevenseg_decoder.vhd
+++ src/sevenseg_decoder.vhd
@@ -9,28 +9,37 @@ use IEEE.STD_LOGIC_1164.ALL;
 --use UNISIM.VComponents.all;
 
 entity sevenseg_decoder is
-    Port ( i_hex : in STD_LOGIC_VECTOR (3 downto 0);
+    Port ( i_Hex : in STD_LOGIC_VECTOR (3 downto 0);
            o_seg_n : out STD_LOGIC_VECTOR (6 downto 0));
 end sevenseg_decoder;
 
 architecture Behavioral of sevenseg_decoder is
+signal w_seg : std_logic_vector(6 downto 0) := "0000000";
 begin
-    with i_hex select
-        o_seg_n <= "1111110" when "0000",
-                   "0110000" when "0001",
-                   "1101101" when "0010",
-                   "1111001" when "0011",
-                   "0110011" when "0100",
-                   "1011011" when "0101",
-                   "1011111" when "0110",
-                   "1110000" when "0111",
-                   "1111111" when "1000",
-                   "1110011" when "1001",
-                   "1110111" when "1010",
-                   "0011111" when "1011",
-                   "0001101" when "1100",
-                   "0111101" when "1101",
-                   "1001111" when "1110",
-                   "1000111" when "1111",
-                   "0000000" when others;
+    with i_Hex select
+    w_seg   <=  "1111110" when x"0",
+                "0110000" when x"1",
+                "1101101" when x"2",
+                "1111001" when x"3",
+                "0110011" when x"4",
+                "1011011" when x"5",
+                "1011111" when x"6",
+                "1110000" when x"7",
+                "1111111" when x"8",
+                "1110011" when x"9",
+                "1110111" when x"A",
+                "0011111" when x"B",
+                "0001101" when x"C",
+                "0111101" when x"D",
+                "1001111" when x"E",
+                "1000111" when x"F",
+                "0000000" when others;
+
+    o_seg_n(0) <= not w_seg(6);
+    o_seg_n(1) <= not w_seg(5);
+    o_seg_n(2) <= not w_seg(4);
+    o_seg_n(3) <= not w_seg(3);
+    o_seg_n(4) <= not w_seg(2);
+    o_seg_n(5) <= not w_seg(1);
+    o_seg_n(6) <= not w_seg(0);
 end Behavioral;
