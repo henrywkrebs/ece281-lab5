60f0e6a86cf4d6d7471b7f9651da6cc696bc9373
henrywkrebs <henrywkrebs@gmail.com>
2026-05-07 19:41:58 -0600

    I can'get the ALU to pass testbench

--- src/ALU.vhd
+++ src/ALU.vhd
@@ -38,33 +38,34 @@ entity ALU is
 end ALU;
 
 architecture Behavioral of ALU is
-    signal w_result : STD_LOGIC_VECTOR (7 downto 0);
-    signal w_sum    : STD_LOGIC_VECTOR (8 downto 0);
+    signal w_result  : std_logic_vector(7 downto 0);
+    signal w_sum9    : std_logic_vector(8 downto 0);
+    signal w_B_op    : std_logic_vector(7 downto 0);
+    signal w_cin     : std_logic;
+    signal w_sum8    : std_logic_vector(7 downto 0);
+    signal w_cout    : std_logic;
+    signal w_carry6  : std_logic;
 begin
 
-    w_sum <= std_logic_vector(signed('0' & i_A) + signed('0' & i_B)) when i_op = "000" else
-             std_logic_vector(signed('0' & i_A) - signed('0' & i_B)) when i_op = "001" else
-             (others => '0');
+    w_cin   <= i_op(0);
+    w_B_op  <= not i_B when i_op(0) = '1' else i_B;
 
-    w_result <= w_sum(7 downto 0) when (i_op = "000" or i_op = "001") else
-                i_A and i_B        when i_op = "010" else
-                i_A or  i_B        when i_op = "011" else
+    w_sum9  <= std_logic_vector(('0' & unsigned(i_A)) + ('0' & unsigned(w_B_op)) + (x"0" & w_cin));
+    w_sum8  <= w_sum9(7 downto 0);
+    w_cout  <= w_sum9(8);
+
+    w_carry6 <= std_logic_vector(('0' & unsigned(i_A(6 downto 0))) + ('0' & unsigned(w_B_op(6 downto 0))) + (x"0" & w_cin))(7);
+
+    w_result <= w_sum8          when (i_op = "000" or i_op = "001") else
+                i_A and i_B     when i_op = "010" else
+                i_A or  i_B     when i_op = "011" else
                 (others => '0');
 
     o_result <= w_result;
 
     o_flags(3) <= w_result(7);
-
-    o_flags(2) <= (not i_A(7) and not i_B(7) and w_result(7))
-                  or (i_A(7) and i_B(7) and not w_result(7))
-                  when i_op = "000" else
-                  (not i_A(7) and i_B(7) and w_result(7))
-                  or (i_A(7) and not i_B(7) and not w_result(7))
-                  when i_op = "001" else
-                  '0';
-
-    o_flags(1) <= w_sum(8) when (i_op = "000" or i_op = "001") else '0';
-
-    o_flags(0) <= '1' when w_result = "00000000" else '0';
+    o_flags(2) <= '1' when w_result = "00000000" else '0';
+    o_flags(1) <= w_cout when (i_op = "000" or i_op = "001") else '0';
+    o_flags(0) <= w_cout xor w_carry6 when (i_op = "000" or i_op = "001") else '0';
 
 end Behavioral;
